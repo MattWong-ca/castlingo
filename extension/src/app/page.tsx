7c69c0e6671a7a84466ef5bdd1d34f4c349830a8
MattWong-ca <matthew.yt.wong@gmail.com>
2024-08-15 19:33:44 -0400

    start extension

---
+++ extension/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 10, height: '400px', width: '300px', backgroundColor: 'lightgreen' }}>
+      <h1>Castlingo</h1>
+      <div>Language learning on Warpcast</div>
+    </div>
+  );
+}
