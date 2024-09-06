9bc34da8ded6b19c16ab8927d9a1f6f51f254c60
MattWong-ca <matthew.yt.wong@gmail.com>
2024-09-06 00:10:17 -0400

    config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,10 +3,22 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     'linea-testnet': {
       url: "https://rpc.goerli.linea.build/",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    'linea-sepolia': {
+      url: "https://rpc.sepolia.linea.build",
       accounts: [process.env.PRIVATE_KEY]
     }
   }
