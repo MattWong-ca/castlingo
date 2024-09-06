207ab3e9b4e8ee68d5886a0836a093f169a0192f
MattWong-ca <matthew.yt.wong@gmail.com>
2024-09-06 00:10:05 -0400

    update script

--- contracts/scripts/deployNFT.js
+++ contracts/scripts/deployNFT.js
@@ -1,7 +1,16 @@
 async function main() {
-    const streakNFT = await ethers.deployContract('StreakNFT');
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const StreakNFT = await ethers.getContractFactory('StreakNFT');
+    
+    // Define the streak requirement
+    const streakRequirement = 7; // or any other number you prefer
+
+    const streakNFT = await StreakNFT.deploy(streakRequirement);
+
     await streakNFT.waitForDeployment();
-    console.log("StreakNFT deployed to:", streakNFT.target);
+    console.log("StreakNFT deployed to:", await streakNFT.getAddress());
 }
 
 main()
